77efbc82c37bafa353198b4b1ec389fa01206187
Xiaonai12226 <penhchetoun@gmail.com>
2026-04-19 00:09:05 +0700

    Create db.js for MongoDB connection

---
+++ server/config/db.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const uri = 'your_mongodb_connection_string';
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
+
+module.exports = mongoose;
